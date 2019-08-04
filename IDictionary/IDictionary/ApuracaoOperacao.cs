8ceb5676bd0b30b649baa6ba9b289c5c287c865d
Gustavo Godinho <gustavogodinho6@msn.com>
2019-08-04 19:43:28 -0300

    primeiro commit

---
+++ IDictionary/IDictionary/ApuracaoOperacao.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IDictionary
+{
+    class ApuracaoOperacao
+    {
+        public Operacao[] ListaOrdenada(Operacao[] operacoes)
+        {
+            
+            try
+            {
+                Operacao[] operacaoSort = operacoes.OrderBy(oper => oper.Id).ToArray();
+
+                return operacaoSort;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+           
+        }
+
+        public IDictionary<string, List<Operacao>> AgruparPorIdentificador(IEnumerable<Operacao> operacoes)
+        {
+            IDictionary<string, List<Operacao>> retorno = new Dictionary<string, List<Operacao>>();
+
+            foreach (var item in operacoes)
+            {
+                if (retorno.ContainsKey(item.Identificador))
+                {
+                    retorno[item.Identificador].Add(item);
+                }
+                else
+                {
+                    retorno.Add(item.Identificador, new List<Operacao>() { item });
+                }
+            }
+
+            return retorno;
+        }
+
+        public IDictionary<string, double> SumarizarPorIdentificador(IEnumerable<Operacao> operacoes)
+        {
+
+            IDictionary<string, double> retorno = new Dictionary<string, double>();
+
+            foreach (var item in operacoes)
+            {
+                if (retorno.ContainsKey(item.Identificador))
+                {
+                    retorno[item.Identificador] += item.Valor;
+                }
+                else
+                {
+                    retorno.Add(item.Identificador, item.Valor);
+                }
+
+            }
+
+            return retorno;
+        }
+    }
+}
---
+++ IDictionary/IDictionary/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operacao[] operacoes = {
+                new Operacao(2, "200", -100.00),
+                new Operacao(3, "100",100.00),
+                new Operacao(4, "300",300.00),
+                new Operacao(1, "100",300.00),
+                new Operacao(7, "400", 400.00),
+                new Operacao(6,"100", -300.00),
+                new Operacao(5, "200", 3000.00)
+            };
+
+
+            ApuracaoOperacao apuracao = new ApuracaoOperacao();
+
+
+            Operacao[] operacoesOrdenadas = apuracao.ListaOrdenada(operacoes);
+
+
+            IDictionary<string,List<Operacao>> operacoesAgrupadas = apuracao.AgruparPorIdentificador(operacoesOrdenadas);
+
+            foreach (var item in operacoesAgrupadas)
+            {
+                Console.WriteLine();
+            }
+
+            IDictionary<string, double> operacoesSumarizadas = apuracao.SumarizarPorIdentificador(operacoesOrdenadas);
+
+            foreach (var item in operacoesSumarizadas)
+            {
+                Console.WriteLine();
+            }
+
+        }
+
+    }
+}
